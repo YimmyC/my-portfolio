d0a6ec6ddab537a7bcb3b7cf08d80263f3077e1a
YimmyC <jim.cassano1@gmail.com>
2023-04-10 17:16:22 -0500

    header updated

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <Header />
-      {/* <HeaderDemo /> */}
+      {/* <Header /> */}
+      <HeaderDemo />
       <Home />
       <Footer />
     </>
--- src/components/HeaderDemo.js
+++ src/components/HeaderDemo.js
@@ -2,17 +2,17 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="bg-gray-800 text-white">
-      <div className="container mx-auto flex justify-between items-center py-4">
-        <h1 className="text-xl font-bold">My Website</h1>
-        <nav className="space-x-4">
-          <a href="/about" className="hover:text-gray-300">
+    <header className="bg-white shadow-md">
+      <div className="container mx-auto flex justify-between items-center py-6">
+        <h1 className="text-3xl font-bold text-gray-900">My Website</h1>
+        <nav className="flex items-center">
+          <a href="/about" className="text-gray-700 font-medium link-animation mx-4 transition duration-300 ease-in-out">
             About
           </a>
-          <a href="/work" className="hover:text-gray-300">
+          <a href="/work" className="text-gray-700 font-medium link-animation mx-4 transition duration-300 ease-in-out">
             Work
           </a>
-          <a href="/contact" className="hover:text-gray-300">
+          <a href="/contact" className="text-gray-700 font-medium link-animation mx-4 transition duration-300 ease-in-out">
             Contact
           </a>
         </nav>
