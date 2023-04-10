65a459ab8ff78f46b1ae910c3d9f3b371ce1601e
YimmyC <jim.cassano1@gmail.com>
2023-04-10 17:42:26 -0500

    font changed

--- src/components/HeaderDemo.js
+++ src/components/HeaderDemo.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-6">
-        <h1 className="text-3xl font-bold text-gray-900">My Website</h1>
+        <h1 className="text-3xl font-bold font-roboto">Jimmy Cassano</h1>
         <nav className="flex items-center">
           <a href="/about" className="text-gray-700 font-medium link-animation mx-4 transition duration-300 ease-in-out">
             About
