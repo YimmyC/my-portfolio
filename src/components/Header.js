e874c2a4a588a2859f29b417b71d8cf77538049e
YimmyC <jim.cassano1@gmail.com>
2023-04-10 16:26:30 -0500

    tailwindcss cli working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header>
-      <h1>Jimmy C's Portfolio</h1>
+      <h1 className="text-3xl font-bold underline p-2">Jimmy C's Portfolio</h1>
       <h2>Coder and Tender of Bars</h2>
       <nav>
         <a href="#about">About</a>
