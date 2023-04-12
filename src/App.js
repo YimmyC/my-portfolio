59d36efbb145ca304c292af6d1be5c5973056890
YimmyC <jim.cassano1@gmail.com>
2023-04-12 10:20:13 -0500

    particles ehhhhh

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import Footer from "./components/Footer";
 import AboutMe from "./components/AboutMe";
 import Work from "./components/Work";
 import { BrowserRouter, Routes, Route, Switch } from "react-router-dom";
+import Particles from "./components/Particles";
 
 function App() {
   return (
-    <>
+    <div>
+      {/* <Particles className="particles" /> */}
+
       <Header />
       <AboutMe />
+
       <Work />
+
       {/* <Home /> */}
       <Footer />
-    </>
+    </div>
   );
 }
 
