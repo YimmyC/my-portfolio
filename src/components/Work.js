b5a48a1a31c7101d45f035eca22a6f6dc8b26c90
YimmyC <jim.cassano1@gmail.com>
2023-04-12 14:35:24 -0500

    fixed blur animation

--- src/components/Work.js
+++ src/components/Work.js
@@ -52,8 +52,8 @@ const Work = () => {
       <div className="flex flex-wrap justify-center items-center w-full max-w-7xl">
         <h1 className=" text-8xl sm:text-5xl font-prompt text-center mb-8">Work</h1>
         {projects.map((project, index) => (
-          <div key={index} className="relative w-full sm:w-screen h-screen" onMouseOver={() => handleMouseOver(index)} onMouseLeave={() => handleMouseLeave()}>
-            <img src={project.image} alt={project.name} className={`absolute inset-0 object-cover object-center ${hoveredIndex === index ? "scale-110" : "scale-100"} transition-all duration-300 ${index === 2 ? "min-h-screen" : ""}`} />
+          <div key={index} className="relative w-full sm:w-screen h-screen overflow-hidden max-h-screen" onMouseOver={() => handleMouseOver(index)} onMouseLeave={() => handleMouseLeave()}>
+            <img src={project.image} alt={project.name} className={`absolute inset-0 w-full h-full object-cover object-center ${hoveredIndex === index ? "filter blur-sm" : ""} transition-all duration-300`} />
             {hoveredIndex === index && (
               <a href={project.link} target="_blank" className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-gray-900 bg-opacity-50 text-white font-semibold text-lg transition-all duration-300">
                 {project.name}
