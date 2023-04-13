14003f80d82864e5c625c22db4b8e7178cd6c421
YimmyC <jim.cassano1@gmail.com>
2023-04-12 19:34:53 -0500

    about me looks good on mobile

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 const AboutMe = () => {
   const [text, setText] = useState("");
   const [isTyping, setIsTyping] = useState(false);
+  const [textShown, setTextShown] = useState(false);
   const typingSpeed = 20; // in milliseconds
 
   useEffect(() => {
@@ -31,15 +32,25 @@ const AboutMe = () => {
     setIsTyping(true);
   };
 
+  useEffect(() => {
+    if (text !== "" && !textShown) {
+      setTextShown(true);
+    }
+  }, [text, textShown]);
+
   return (
     <div id="about" className="z-10 relative flex flex-col justify-center items-center bg-transparent shadow-md rounded-md p-6 w-full h-screen mx-auto">
-      <h2 className="font-prompt text-9xl font-bold mb-4 text-center">About Me</h2>
-      {!isTyping && (
+      <h2 className="font-prompt lg:text-9xl md:text-9xl text-6xl font-bold mb-4 text-center" style={{ padding: "0 1rem" }}>
+        About Me
+      </h2>
+      {!textShown && (
         <span className="font-prompt text-black underline cursor-pointer" onClick={handleClick}>
           Click to read
         </span>
       )}
-      <p className="font-prompt text-gray-700 text-2xl mb-4">{text}</p>
+      <p className="font-prompt text-black text-2xl mb-4" style={{ textShadow: "1px 1px 20px #4f4e4e", fontSize: "1.5rem" }}>
+        {text}
+      </p>
     </div>
   );
 };
